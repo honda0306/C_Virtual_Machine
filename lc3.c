b5ecc2d1092e4553b4ddf8151524dcddd59b5a0c
Chris Honda <christopher.honda92@gmail.com>
2019-02-05 21:57:45 -0800

    Almost finished with operators

--- lc3.c
+++ lc3.c
@@ -65,12 +65,153 @@ uint16_t memory[UINT16_MAX];
 /* Register Storage */
 uint16_t reg[R_COUNT];
 
+/* Functions */
+/* Sign Extend */
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+    if ((x >> (bit_count -1)) & 1) {
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
+
+/* Swap */
+uint16_t swap16(uint16_t x)
+{
+    return (x << 8) | (x >> 8);
+}
+
+/* Update Flags */
+void update_flags(uint16_t r)
+{
+    if (reg[r] == 0)
+    {
+        reg[R_COND] = FL_ZRO;
+    }
+    else if (reg[r] >> 15) /* a 1 in the left-most bit indicates negative */
+    {
+        reg[R_COND] = FL_NEG;
+    }
+    else
+    {
+        reg[R_COND] = FL_POS;
+    }
+}
+
+/* Read Image File */
+void read_image_file(FILE* file)
+{
+    /* Origin indicates the location in memory the image should be placed */
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    /* Only one fread needed since we know max file size */
+    uint16_t max_read = UINT16_MAX - origin;
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    /* Swap to little endian */
+    while (read-- > 0)
+    {
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+/* Read Image */
+int reac_image(const char* image_path)
+{
+    FILE* file = fopen(image_path, "rb");
+    if (!file) { return 0; };
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}
+
+/* Check Key */
+uint16_t check_key()
+{
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, & readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
+void mem_write(uint16_t address, uint16_t val)
+{
+    memory[address] = val;
+}
+
+uint16_t mem_read(uint16_t address)
+{
+    if (address == MR_KBSR)
+    {
+        if (check_key())
+        {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        }
+        else{
+            memory[MR_KBSR] = 0;
+        }
+    }
+    return memory[address];
+}
+
+/* Input Buffering */
+struct termios original_tio;
+
+void disable_input_buffering()
+{
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+/* Handle Interrupt */
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
+
 
 /* Main function loop */
 int main(int argc, const char* argv[])
 {
-    {Load Arguments, 12}
-    {Setup, 12}
+    /* Load Arguments */
+    if (argc < 2)
+    {
+        /* show usage string */
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+
+    for (int j = 1; j < argc; ++j)
+    {
+        if (!read_image(argv[j]))
+        {
+            printf("Failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+
+    /* Setup */
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
 
     /* set the PC to starting position */
     /* 0x3000 is the default */
@@ -87,22 +228,96 @@ int main(int argc, const char* argv[])
         switch (op)
         {
             case OP_ADD:
-                {ADD, 6}
+                {
+                    /* Destination register (DR) */
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    /* First Operand (SR1) */
+                    uint16_t r1 = (instr >> 6) & 0x7;
+                    /* Whether we are in immediate mode */
+                    uint16_t imm_flag = (instr >> 5) & 0x1;
+
+                    if (imm_flag)
+                    {
+                        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                        reg[r0] = reg[r1] + imm5;
+                    }
+                    else 
+                    {
+                        uint16_t r2 = instr & 0x7;
+                        reg[r0] = reg[r1] + reg[r2];
+                    }
+                    update_flags(r0);
+                }
                 break;
+
             case OP_AND:
-                {AND, 7}
+                {
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    uint16_t r1 = (instr >> 6) & 0x7;
+                    uint16_t imm_flag = (instr >> 5) & 0x1;
+
+                    if (imm_flag)
+                    {
+                        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                        reg[r0] = reg[r1] & imm_5;
+                    }
+                    else
+                    {
+                        uint16_t r2 = instr & 0x7;
+                        reg[r0] = reg[r1] & reg[r2];
+                    }
+                    update_flags(r0);
+                }
                 break;
+
             case OP_NOT:
-                {NOT, 7}
+                {
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    uint16_t r1 = (instr >> 6) & 0x7;
+
+                    reg[r0] = ~reg[r1];
+                    update_flags(r0);
+                }
+
                 break;
+
             case OP_BR:
-                {BR, 7}
+                {
+                    uint16_t pc_offset = sign_extend((instr) & 0x1ff, 9);
+                    uint16_t cond_flag = (instr >> 9) & 0x7;
+                    if (cond_flag & reg[R_COND])
+                    {
+                        reg[R_PC] += pc_offset;
+                    }
+                }
+
                 break;
+
             case OP_JMP:
-                {JMP, 7}
+                {
+                    uint16_t r1 = (instr >> 6) & 0x7;
+                    reg[R_PC] = reg[r1];
+                }
+
                 break;
+
             case OP_JSR:
-                {JSR, 7}
+                {
+                    uint16_t r1 = (instr >> 6) & 0x7;
+                    uint16_t long_pc_offset = sign_extend(instr & 0x7ff, 11);
+                    uint16_t long_flag = (instr >> 11) & 1;
+
+                    reg[R_R7] = reg[R_PC];
+                    if (long_flag)
+                    {
+                        reg[R_PC] += long_pc_offset;
+                    }
+                    else
+                    {
+                        reg[R_PC] = reg[r1];
+                    }
+                    break;
+                }
                 break;
             case OP_LD:
                 {LD, 7}
